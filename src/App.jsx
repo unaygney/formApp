27113e30bb68aaec5c05cc41e8cc3b8467514167
guneyunay <mesafe18@hotmail.com>
2023-07-20 12:46:58 +0300

    add api and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TaskCreate from "./components/TaskCreate";
-import TaskList from './components/TaskList'
-
+import TaskList from "./components/TaskList";
+import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const createTask = (title, desc) => {
-    const createdTasks = [
-      ...tasks,
-      {
-        title,
-        desc,
-        id: Math.round(Math.random() * 99999),
-      }
-    ];
-    setTasks(createdTasks)
+  
 
+  const createTask = async (title, desc) => {
+    const response = await axios.post("http://localhost:5170/tasks", {
+      title,
+      desc,
+    });
+    const createdTasks = [...tasks, response.data];
+    setTasks(createdTasks);
   };
- const deleteTask = (id) => {
-const tasksAfterDeleting = tasks.filter((task) => {
-return task.id !==id
-})
+  const deleteTask =  async(id) => {
+    const response = await axios.delete(`http://localhost:5170/tasks/${id}`)
+    const tasksAfterDeleting = tasks.filter((task) => {
+      return task.id !== id;
+    });
 
-setTasks(tasksAfterDeleting)
- }
+    setTasks(tasksAfterDeleting);
+  };
+  const fetchData = async () => {
+    const response = await axios.get('http://localhost:5170/tasks')
+    setTasks(response.data)
+  }
+
+
+  useEffect(() => {
+    fetchData()
+    
+  }, [])
+  
 
   return (
     <div>
